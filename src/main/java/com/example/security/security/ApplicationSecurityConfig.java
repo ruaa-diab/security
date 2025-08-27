9db7ba12cb8bfc6a8cd92815e145e579aa2586fb
Ruaa Diab <Ruaa.Diab@exalt.ps>
2025-08-27 16:36:44 +0300

    adding starter security djjjep

---
+++ src/main/java/com/example/security/security/ApplicationSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.security.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+
+    }
+
+
+
+
+}
+
+
