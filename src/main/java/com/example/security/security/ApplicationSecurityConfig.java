b15848e35e685eb2037241099b8b0642465443bd
Ruaa Diab <Ruaa.Diab@exalt.ps>
2025-08-31 10:20:29 +0300

    adding starter security

--- src/main/java/com/example/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/security/ApplicationSecurityConfig.java
@@ -1,21 +1,61 @@
 package com.example.security.security;
 
 
+import com.sun.source.tree.ReturnTree;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig {
-    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       http
+               .authorizeRequests()
+               .requestMatchers("/*","index","/css/*")
+               .permitAll()
+               .anyRequest()
+               .authenticated()
+               .and()
+               .httpBasic();
+       return http.build();
 
     }
 
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        UserDetails ruru = User.builder()
+                .username("ruaa-diab")
+                .password(this.passwordEncoder.encode("12345"))
+                .roles("STUDENT")
+                .build();
+        UserDetails rose= User.builder()
+                .username("rose")
+                .password(this.passwordEncoder.encode("12340"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(ruru,rose);
+    }
 
 
-
+    public PasswordEncoder getPasswordEncoder() {
+        return passwordEncoder;
+    }
 }
 
 
